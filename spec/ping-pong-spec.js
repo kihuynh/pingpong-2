39eb0faeb0b5793a1372bf57c8e1e4fcdb21739d
Anna Marie Cooper and Kimberly Huynh <kimberly@>
2018-03-13 11:58:48 -0700

    pass two karma specs

--- spec/ping-pong-spec.js
+++ spec/ping-pong-spec.js
@@ -1,7 +1,18 @@
 import { pingPong } from '../src/ping-pong.js';
 describe('pingPong', function() {
+
+  // var game;
+  //
+  // beforeEach(function(){
+  //   game = new pingPong(0);
+  // });
+
   it('should say "ping" if divisible by % 3', function() {
     var game = new pingPong(3)
-    expect(pingPong[2]).toEqual("ping")
+    expect(game[2]).toEqual("ping");
+  });
+  it('should say "pong" if divisible by % 5', function() {
+    var game = new pingPong(5)
+    expect(game[4]).toEqual("pong");
   });
 });
